d45c25ad33f00313471559c432eed1854392d13c
hyu1335 <hyuham1335@gmail.com>
2025-12-05 15:02:43 +0900

    step1: 사칙연산 기호 입력 및 계산결과 출력 구현

--- src/step1/Calculator.java
+++ src/step1/Calculator.java
@@ -8,9 +8,9 @@ public class Calculator {
 
         Scanner sc = new Scanner(System.in);
         int a, b;
+        char operator;
 
-        while (true)
-        {
+        while (true) {
             try {
                 System.out.print("첫번째 숫자를 입력해 주세요: ");
                 a = sc.nextInt();
@@ -25,8 +25,7 @@ public class Calculator {
             }
         }
 
-        while (true)
-        {
+        while (true) {
             try {
                 System.out.print("두번째 숫자를 입력해 주세요: ");
                 b = sc.nextInt();
@@ -38,6 +37,42 @@ public class Calculator {
             } catch (InputMismatchException e) {
                 System.out.println("0 이상의 정수만 입력 가능합니다.");
                 sc.nextLine();
+            }
+        }
+
+        boolean operateCheck = true;
+
+        while (operateCheck) {
+            operateCheck = false;
+
+            System.out.print("사칙연산 기호를 입력해 주세요: ");
+            operator = sc.next().charAt(0);
+
+            switch (operator) {
+                case '+':
+                    System.out.println("연산 결과: " + (a + b));
+                    break;
+
+                case '-':
+                    System.out.println("연산 결과: " + (a - b));
+                    break;
+
+                case '*':
+                    System.out.println("연산 결과: " + (a * b));
+                    break;
+
+                case '/':
+                    try {
+                        System.out.println("연산 결과: " + (a / b));
+                    } catch (ArithmeticException e) {
+                        System.out.println("0으로 나눌 수 없습니다.");
+                    }
+                    break;
+
+                default:
+                    System.out.println("올바른 사칙연산 기호를 입력해 주세요");
+                    operateCheck = true;
+                    break;
             }
         }
     }
