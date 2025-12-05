011b18b58cc585e523194165f52ab64d68909799
hyu1335 <hyuham1335@gmail.com>
2025-12-05 15:56:23 +0900

    step1: 반복문을 이용한 연속적인 계산 구현

--- src/step1/Calculator.java
+++ src/step1/Calculator.java
@@ -6,73 +6,83 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        int a, b;
-        char operator;
+        boolean loopCheck = true;
 
-        while (true) {
-            try {
-                System.out.print("첫번째 숫자를 입력해 주세요: ");
-                a = sc.nextInt();
-                if (a < 0) {
-                    System.out.println("0 이상의 정수만 입력 가능합니다.");
-                } else {
-                    break;
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("0 이상의 정수만 입력 가능합니다.");
-                sc.nextLine();
-            }
-        }
+        while(loopCheck){
+            Scanner sc = new Scanner(System.in);
+            int a, b;
+            char operator;
+            boolean operateCheck = true;
 
-        while (true) {
-            try {
-                System.out.print("두번째 숫자를 입력해 주세요: ");
-                b = sc.nextInt();
-                if (b < 0) {
-                    System.out.println("0 이상의 정수만 입력 가능합니다.");
-                } else {
-                    break;
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("0 이상의 정수만 입력 가능합니다.");
-                sc.nextLine();
-            }
-        }
-
-        boolean operateCheck = true;
-
-        while (operateCheck) {
-            operateCheck = false;
-
-            System.out.print("사칙연산 기호를 입력해 주세요: ");
-            operator = sc.next().charAt(0);
-
-            switch (operator) {
-                case '+':
-                    System.out.println("연산 결과: " + (a + b));
-                    break;
-
-                case '-':
-                    System.out.println("연산 결과: " + (a - b));
-                    break;
-
-                case '*':
-                    System.out.println("연산 결과: " + (a * b));
-                    break;
-
-                case '/':
-                    try {
-                        System.out.println("연산 결과: " + (a / b));
-                    } catch (ArithmeticException e) {
-                        System.out.println("0으로 나눌 수 없습니다.");
+            while (true) {
+                try {
+                    System.out.print("첫번째 숫자를 입력해 주세요: ");
+                    a = sc.nextInt();
+                    if (a < 0) {
+                        System.out.println("0 이상의 정수만 입력 가능합니다.");
+                    } else {
+                        break;
                     }
-                    break;
+                } catch (InputMismatchException e) {
+                    System.out.println("0 이상의 정수만 입력 가능합니다.");
+                    sc.nextLine();
+                }
+            }
 
-                default:
-                    System.out.println("올바른 사칙연산 기호를 입력해 주세요");
-                    operateCheck = true;
-                    break;
+            while (true) {
+                try {
+                    System.out.print("두번째 숫자를 입력해 주세요: ");
+                    b = sc.nextInt();
+                    if (b < 0) {
+                        System.out.println("0 이상의 정수만 입력 가능합니다.");
+                    } else {
+                        break;
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("0 이상의 정수만 입력 가능합니다.");
+                    sc.nextLine();
+                }
+            }
+
+            while (operateCheck) {
+                operateCheck = false;
+
+                System.out.print("사칙연산 기호를 입력해 주세요: ");
+                operator = sc.next().charAt(0);
+
+                switch (operator) {
+                    case '+':
+                        System.out.println("연산 결과: " + (a + b));
+                        break;
+
+                    case '-':
+                        System.out.println("연산 결과: " + (a - b));
+                        break;
+
+                    case '*':
+                        System.out.println("연산 결과: " + (a * b));
+                        break;
+
+                    case '/':
+                        try {
+                            System.out.println("연산 결과: " + (a / b));
+                        } catch (ArithmeticException e) {
+                            System.out.println("0으로 나눌 수 없습니다.");
+                        }
+                        break;
+
+                    default:
+                        System.out.println("올바른 사칙연산 기호를 입력해 주세요");
+                        operateCheck = true;
+                        break;
+                }
+            }
+            sc.nextLine();
+
+            System.out.println("계산을 종료할려면 exit을 입력해 주세요");
+            String exitCheck = sc.nextLine();
+            if (exitCheck.equals("exit")) {
+                loopCheck = false;
             }
         }
     }
