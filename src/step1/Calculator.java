6e0d14f8da2bca83f366a0d228100f37773d1c92
hyu1335 <hyuham1335@gmail.com>
2025-12-05 14:34:30 +0900

    step1: 양의 정수 2개 입력받기 구현

--- src/step1/Calculator.java
+++ src/step1/Calculator.java
@@ -1,7 +1,44 @@
 package step1;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+
+        while (true)
+        {
+            try {
+                System.out.print("첫번째 숫자를 입력해 주세요: ");
+                a = sc.nextInt();
+                if (a < 0) {
+                    System.out.println("0 이상의 정수만 입력 가능합니다.");
+                } else {
+                    break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("0 이상의 정수만 입력 가능합니다.");
+                sc.nextLine();
+            }
+        }
+
+        while (true)
+        {
+            try {
+                System.out.print("두번째 숫자를 입력해 주세요: ");
+                b = sc.nextInt();
+                if (b < 0) {
+                    System.out.println("0 이상의 정수만 입력 가능합니다.");
+                } else {
+                    break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("0 이상의 정수만 입력 가능합니다.");
+                sc.nextLine();
+            }
+        }
     }
 }
